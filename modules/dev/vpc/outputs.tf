53bfd0c6fda1aa9b2b5a42bdaaf3c6bbc2f36942
MichaelCassidyPS <michael-cassidy@pluralsight.com>
2024-02-06 16:07:14 +0000

    edited vpc outputs.tf

--- modules/dev/vpc/outputs.tf
+++ modules/dev/vpc/outputs.tf
@@ -1,10 +1,5 @@
 # --- vpc/outputs.tf ---
 
-output "vpc_id" {
-  description = "ID of the VPC"
-  value       = aws_vpc.instance_vpc.id
-}
-
 output "subnet_id" {
   description = "ID of subnet"
   value       = aws_subnet.instance_subnet.id
